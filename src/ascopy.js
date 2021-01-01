d0d266c02331c88193d0b03877b325315b526ada
Kamesuta <kamesuta@gmail.com>
2021-01-01 11:04:05 +0900

    ascopy

---
+++ src/ascopy.js
@@ -0,0 +1,89 @@
+const Discord = require('discord.js')
+const url = require('url')
+
+module.exports = function (client) {
+    client.on('ready', () => {
+        console.log(`Logged in as ${client.user.tag}!`)
+
+        client.api.applications(client.user.id).commands.post({
+            data: {
+                name: "ascopy",
+                description: "他人のメッセージをそっくりコピーできます。",
+                content: "AsCopy",
+                options: [
+                    {
+                        name: "url",
+                        description: "メッセージURL",
+                        type: 3,
+                        required: true,
+                    }
+                ]
+            }
+        })
+
+        client.ws.on('INTERACTION_CREATE', async interaction => {
+            const command = interaction.data.name.toLowerCase()
+
+            if (command === 'ascopy') {
+                const channel = await client.channels.fetch(interaction.channel_id)
+                const {ok, reason} = await ascopy(channel, interaction.data.options[0].value)
+
+                client.api.interactions(interaction.id, interaction.token).callback.post({
+                    data: ok
+                        ? {
+                            type: 2
+                        }
+                        : {
+                            type: 4,
+                            data: {
+                                content: '❌ ' + reason
+                            }
+                        }
+                })
+            }
+        })
+    })
+
+    async function ascopy(channel, argUrl) {
+        if (!channel.guild.me.permissionsIn(channel).has(Discord.Permissions.FLAGS.MANAGE_WEBHOOKS))
+            return {ok: false, reason: "このチャンネルでは /ascopy は使用できません"}
+
+        const cUrl = argUrl
+        if (cUrl === null)
+            return {ok: false, reason: "メッセージURLを指定してください"}
+        const cPath = url.parse(cUrl).path
+        if (cPath === null)
+            return {ok: false, reason: "メッセージURLが不正です"}
+        const cMatch = cPath.match(/^\/channels\/(\d+)\/(\d+)\/(\d+)/)
+        if (cMatch === null)
+            return {ok: false, reason: "メッセージURLが不正です"}
+        const [_, cGuildId, cChannelId, cMessageId] = cMatch
+        const cChannel = await client.channels.fetch(cChannelId)
+        if (cChannel == null)
+            return {ok: false, reason: "Botが取得できないチャンネルのメッセージです"}
+        const cMessage = await cChannel.messages.fetch(cMessageId)
+        if (cMessage == null)
+            return {ok: false, reason: "メッセージがみつからない、または削除されています"}
+
+        let username = cMessage.member?.displayName ?? cMessage.author.username
+        if (cMessage.author.bot)
+            username += ' [Bot]'
+
+        const message = {
+            content: cMessage.content,
+            username,
+            avatarURL: cMessage.author.avatarURL(),
+            embeds: cMessage.embeds,
+            files: Array.from(cMessage.attachments, ([key, e]) => {
+                return {
+                    name: e.name,
+                    attachment: e.attachment,
+                }
+            })
+        }
+        const hooks = await channel.fetchWebhooks()
+        hooks.filter(e => e.name === 'AsSay').forEach(e => e.send(message))
+
+        return {ok: true}
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,141 +1,68 @@
-const Discord = require('discord.js');
-const Parser = require('discord-command-parser');
-const config = require('config');
-const url = require('url');
+const Discord = require('discord.js')
+const Parser = require('discord-command-parser')
+const config = require('config')
+const url = require('url')
+const ascopy = require('./ascopy')
 
-const prefix = "/";
-const client = new Discord.Client();
+const prefix = "/"
+const client = new Discord.Client()
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-
-    client.api.applications(client.user.id).commands.post({
-        data: {
-            name: "assay",
-            description: "他人になりすまして発言できます",
-            content: "AsSay",
-            options: [
-                {
-                    name: "username",
-                    description: "ID、表示名、名前、名前#タグ",
-                    type: 3,
-                    required: true,
-                },
-                {
-                    name: "user",
-                    description: "ユーザー",
-                    type: 6,
-                    required: true,
-                },
-            ],
-        }
-    });
-
-    client.ws.on('INTERACTION_CREATE', async interaction => {
-        const command = interaction.data.name.toLowerCase();
-        const args = interaction.data.options;
-
-        if (command === 'assay'){
-            client.api.interactions(interaction.id, interaction.token).callback.post({
-                data: {
-                    type: 4,
-                    data: {
-                        content: "hello world!!!"
-                    }
-                }
-            })
-        }
-    });
-});
+ascopy(client)
 
 client.on('message', async msg => {
-    const parsed = Parser.parse(msg, prefix);
+    const parsed = Parser.parse(msg, prefix)
     if (!parsed.success)
-        return;
-    if (parsed.command !== 'as' && parsed.command !== 'ascopy')
+        return
+    if (parsed.command !== 'as')
         return
 
     if (!msg.guild.me.permissionsIn(msg.channel).has(Discord.Permissions.FLAGS.MANAGE_WEBHOOKS))
-        return;
+        return
 
     let message
 
-    if (parsed.command === 'ascopy') {
-        const cUrl = parsed.reader.getString()
-        if (cUrl === null)
-            return
-        const cPath = url.parse(cUrl).path
-        if (cPath === null)
-            return
-        const cMatch = cPath.match(/^\/channels\/(\d+)\/(\d+)\/(\d+)/)
-        if (cMatch === null)
-            return
-        const [_, cGuildId, cChannelId, cMessageId] = cMatch
-        const cChannel = await client.channels.fetch(cChannelId)
-        if (cChannel == null)
-            return
-        const cMessage = await cChannel.messages.fetch(cMessageId)
-        if (cMessage == null)
-            return
+    const userStr = parsed.reader.getString(true)
+    const userId = parsed.reader.getUserID()
 
-        if (cMessage.author.bot)
-            return;
+    let user = null
+    let member = null
+    if (userId != null)
+        user = await client.users.fetch(userId)
+    if (user == null) {
+        const members = await msg.guild.members.fetch()
+        member = members.find(e => e.user.tag === userStr)
+        if (member == null)
+            member = members.find(e => e.displayName === userStr)
+        if (member == null)
+            member = members.find(e => e.user.username === userStr)
+    }
+    if (user != null)
+        member = await msg.guild.members.resolve(user)
+    else if (member != null)
+        user = member.user
 
-        message = {
-            content: cMessage.content,
-            username: cMessage.member?.displayName ?? cMessage.author.username,
-            avatarURL: cMessage.author.avatarURL(),
-            nonce: msg.id,
-            embeds: cMessage.embeds,
-            files: Array.from(cMessage.attachments, ([key, e]) => {
-                return {
-                    name: e.name,
-                    attachment: e.attachment,
-                }
-            })
-        }
-    } else {
-        const userStr = parsed.reader.getString(true);
-        const userId = parsed.reader.getUserID();
+    if (user === null)
+        return
 
-        let user = null
-        let member = null
-        if (userId != null)
-            user = await client.users.fetch(userId);
-        if (user == null) {
-            const members = await msg.guild.members.fetch();
-            member = members.find(e => e.user.tag === userStr);
-            if (member == null)
-                member = members.find(e => e.displayName === userStr);
-            if (member == null)
-                member = members.find(e => e.user.username === userStr);
-        }
-        if (user != null)
-            member = await msg.guild.members.resolve(user);
-        else if (member != null)
-            user = member.user;
+    const content = parsed.reader.getRemaining()
+    if (content === null)
+        return
 
-        if (user === null)
-            return;
-        if (user.bot)
-            return;
+    let username = member?.displayName ?? user.username
+    if (user.bot)
+        username += ' [Bot]'
 
-        const content = parsed.reader.getRemaining()
-        if (content === null)
-            return
-
-        message = {
-            content,
-            username: member?.displayName ?? user.username,
-            avatarURL: user.avatarURL(),
-            nonce: msg.id
-        }
+    message = {
+        content,
+        username,
+        avatarURL: user.avatarURL(),
+        nonce: msg.id
     }
 
-    const hooks = await msg.channel.fetchWebhooks();
+    const hooks = await msg.channel.fetchWebhooks()
     hooks.filter(e => e.name === 'AsSay').forEach(e => {
-        e.send(message);
-    });
-});
+        e.send(message)
+    })
+})
 
-client.login(config.token);
+client.login(config.token)
